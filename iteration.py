cf24b038cdfe27f0a2285f8a2e72014558827341
Jack Wiegman <wiegmanja@s.dcsdk12.org>
2017-10-23 08:06:57 -0600

    Print names with scores

--- iteration.py
+++ iteration.py
@@ -10,3 +10,8 @@ def iterate(list):
 	# for each loop
 	for item in list:
 		print item
+
+def print_scores(names, scores):
+	for i in range(0, len(names)):
+		#print names[i] , "scored" , scores[i]
+		print "%s scored %s on the test" % (names[i], scores[i])
